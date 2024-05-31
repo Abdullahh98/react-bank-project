c8abab5094b0c65bc52c325b198c62dbefa73103
fatemahalzahraa <fatemah.a.baqer@gmail.com>
2024-05-31 03:20:48 +0300

    solved view all transactions with color change

--- src/components/TransactionItem.js
+++ src/components/TransactionItem.js
@@ -3,14 +3,16 @@ import React from "react";
 const TransactionItem = ({ amount, date, type }) => {
   return (
     <div className=" flex w-[650px] h-full justify-center items-center rounded-md shadow-lg shadow-slate-300">
-      <div className=" w-full text-blue-500 flex justify-center ">{amount}</div>
-      <div className=" w-full text-gray-500 flex justify-center">{date}</div>
-
       {type == "deposit" ? (
-        <div className=" w-full text-green-500 flex justify-center">{type}</div>
+        <div className=" w-full text-green-500 flex justify-center ">
+          {amount}
+        </div>
       ) : (
-        <div className=" w-full text-red-500 flex justify-center">{type}</div>
+        <div className=" w-full text-red-500 flex justify-center">{amount}</div>
       )}
+
+      <div className=" w-full text-gray-500 flex justify-center">{date}</div>
+      <div className=" w-full text-black flex justify-center">{type}</div>
     </div>
   );
 };
